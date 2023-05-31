87cb815e694a1479486626295fdd6478da1fa27c
Pravesh <pritamyadav70923@gmail.com>
2023-05-31 13:16:49 +0530

    id correction for form-Input

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -34,7 +34,7 @@ const AvailableMeals = () => {
       <Card>
         <ul>
           {DUMMY_MEALS.map((meal) => (
-            <MealItem
+            <MealItem id={meal.id}
               key={meal.id}
               name={meal.name}
               description={meal.description}
