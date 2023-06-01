283e1cae001ad406d70c307e1bd7ee6f2cc79d33
Pravesh <pritamyadav70923@gmail.com>
2023-06-01 10:39:13 +0530

    ref and forward ref to mealItemForm

--- src/components/Meals/MealItem/Input.js
+++ src/components/Meals/MealItem/Input.js
@@ -1,9 +1,12 @@
-import classes from "./Input.module.css"
-const Input=(props)=>{
-  return <div className={classes.input}>
-    <label htmlFor={props.input.id}>{props.label}</label>
-    <input  {...props.input}></input>
-  </div>
-}
+import React from "react";
+import classes from "./Input.module.css";
+const Input = React.forwardRef((props, ref) => {
+  return (
+    <div className={classes.input}>
+      <label htmlFor={props.input.id}>{props.label}</label>
+      <input ref={ref} {...props.input}></input>
+    </div>
+  );
+});
 
-export default Input
+export default Input;
